fe693c7cfaaa8b694c3f135687be733da402483b
dewansh-singh-chandel <119128879+dewansh-singh-chandel@users.noreply.github.com>
2023-10-20 23:41:20 +0530

    Add files via upload

--- connect4.py
+++ connect4.py
@@ -1,10 +1,10 @@
 ROWS = 4
 COLS = 5
 
-def init_board():
-    return [[' ' for _ in range(COLS)] for _ in range(ROWS)]
+board = [[' ' for _ in range(COLS)] for _ in range(ROWS)]
 
-def print_board(board):
+
+def print_board():
     print()
     for row in board:
         print("|", end=" ")
@@ -14,15 +14,18 @@ def print_board(board):
     print("|---|---|---|---|---|")
     print("| 1 | 2 | 3 | 4 | 5 |")
 
-def is_valid_move(board, col):
+
+def is_valid_move(col):
     return col >= 0 and col < COLS and board[0][col] == ' '
 
-def drop_disc(board, col, player):
+
+def drop_disc(col, player):
     for row in range(ROWS - 1, -1, -1):
         if board[row][col] == ' ':
             board[row][col] = player
             return row
     return -1
+
 
 def check_win(board, player):
     def check_direction(dx, dy):
@@ -43,66 +46,62 @@ def check_win(board, player):
             return True
     return False
 
-def is_board_full(board):
+
+def is_board_full():
     for col in board[0]:
         if col == ' ':
             return False
     return True
 
+
 def other_player(player):
     return 'O' if player == 'X' else 'X'
 
-def best_move(board, player, memo):
+
+def best_move(board, player):
     if check_win(board, player):
         return {'score': 1}
     elif check_win(board, other_player(player)):
         return {'score': -1}
-    elif is_board_full(board):
+    elif is_board_full():
         return {'score': 0}
 
     best_moove = None
     best_score = float('-inf')
 
     for col in range(COLS):
-        if is_valid_move(board, col):
-            row = drop_disc(board, col, player)
-            key = str(board)
-            if key in memo:
-                score = memo[key]
-            else:
-                score = -best_move(board, other_player(player), memo)['score']
-                memo[key] = score
+        if is_valid_move(col):
+            row = drop_disc(col, player)
+            score = -best_move(board, other_player(player))['score']
             board[row][col] = ' '
 
             if score == 1:
-                return {'col': col, 'score': score}
+                return {'row': row, 'col': col, 'score': score}
 
             if score > best_score:
                 best_score = score
-                best_moove = {'col': col, 'score': score}
+                best_moove = {'row': row, 'col': col, 'score': score}
 
     return best_moove
 
+
 def main():
-    board = init_board()
     player = 'X'
-    memo = {}
     while True:
-        print_board(board)
+        print_board()
 
         if player == 'X':
             print("Player X, enter your move (1-5): ")
             col = int(input()) - 1
 
-            if is_valid_move(board, col):
-                row = drop_disc(board, col, player)
-                board[row][col] = 'X'
+            if is_valid_move(col):
+                row = drop_disc(col, player)
                 if check_win(board, player):
-                    print_board(board)
+                    print_board()
                     print("Player X wins!")
                     break
-                elif is_board_full(board):
-                    print_board(board)
+                elif is_board_full():
+                    print_board()
                     print("It's a draw!")
                     break
                 player = 'O'
@@ -110,18 +109,19 @@ def main():
                 print("Invalid move. Try again.")
         else:
             print("Computer's move...")
-            col = best_move(board, player, memo)['col']
-            row = drop_disc(board, col, player)
-            board[row][col] = 'O'
+            # col = best_move(board, player)
+            col = best_move(board, player)['col']
+            row = drop_disc(col, player)
             if check_win(board, player):
-                print_board(board)
+                print_board()
                 print("Computer (Player O) wins!")
                 break
-            elif is_board_full(board):
-                print_board(board)
+            elif is_board_full():
+                print_board()
                 print("It's a draw!")
                 break
             player = 'X'
+
 
 if __name__ == "__main__":
     main()
